0800237624e60975ea6def5d0a99b07572a3a877
iCHAIT <cgupta319@gmail.com>
2015-12-27 17:59:43 +0530

    retrieve all movie names

---
+++ moviemon.py
@@ -0,0 +1,22 @@
+import os
+
+path = "/Users/chaitanyagupta/Movies/Movies/English/UnWatched/"
+
+EXT = (".3g2 .3gp .3gp2 .3gpp .60d .ajp .asf .asx .avchd .avi .bik .bix"
+       ".box .cam .dat .divx .dmf .dv .dvr-ms .evo .flc .fli .flic .flv"
+       ".flx .gvi .gvp .h264 .m1v .m2p .m2ts .m2v .m4e .m4v .mjp .mjpeg"
+       ".mjpg .mkv .moov .mov .movhd .movie .movx .mp4 .mpe .mpeg .mpg"
+       ".mpv .mpv2 .mxf .nsv .nut .ogg .ogm .omf .ps .qt .ram .rm .rmvb"
+       ".swf .ts .vfw .vid .video .viv .vivo .vob .vro .wm .wmv .wmx"
+       ".wrap .wvx .wx .x264 .xvid")
+
+EXT = tuple(EXT.split())
+
+
+# movies = []
+
+for root, dirs, files in os.walk(path):
+    for name in files:
+    	ext = os.path.splitext(name)[1]
+    	if ext in EXT:
+    		print name

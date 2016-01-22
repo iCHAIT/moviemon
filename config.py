d4a0f494b1ddc911f0efefa60e5d00ca16c6f685
iCHAIT <cgupta319@gmail.com>
2016-01-22 23:45:47 +0530

    modular changes

---
+++ config.py
@@ -0,0 +1,2 @@
+# The paths to your movie directory
+PATHS = ["/Users/chaitanyagupta/Movies/Movies/English/UnWatched/"]
--- moviemon.py
+++ moviemon.py
@@ -1,6 +1,11 @@
 import os
+import urllib2
 
-path = "/Users/chaitanyagupta/Movies/Movies/English/UnWatched/"
+from guessit import guess_file_info
+
+from config import PATHS
+
+OMDB_URL = 'http://www.omdbapi.com/?'
 
 EXT = (".3g2 .3gp .3gp2 .3gpp .60d .ajp .asf .asx .avchd .avi .bik .bix"
        ".box .cam .dat .divx .dmf .dv .dvr-ms .evo .flc .fli .flic .flv"
@@ -12,11 +17,35 @@ EXT = (".3g2 .3gp .3gp2 .3gpp .60d .ajp .asf .asx .avchd .avi .bik .bix"
 
 EXT = tuple(EXT.split())
 
+movies = []
 
-# movies = []
 
-for root, dirs, files in os.walk(path):
-    for name in files:
-    	ext = os.path.splitext(name)[1]
-    	if ext in EXT:
-    		print name
+def scan_dir(path):
+    for root, dirs, files in os.walk(path):
+            for name in files:
+                path = os.path.join(root, name)
+                if os.path.getsize(path) > (25*1024*1024):
+                    ext = os.path.splitext(name)[1]
+                    if ext in EXT:
+                        print name
+                        get_movie_info(name)
+                        print "\n"
+                        # movies.append(movie_name)
+
+# print movies
+
+
+# def omdb():
+#   """ Fetch data from OMDB API. """
+#     url = OMDB_URL +
+
+
+def get_movie_info(name):
+    """Find movie information"""
+    movie_name = guess_file_info(name)
+    print movie_name
+
+
+if __name__ == '__main__':
+    for path in PATHS:
+        scan_dir(path)

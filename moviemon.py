3196986bd7768fa09c84918e7cf793b66fe82cd3
iCHAIT <cgupta319@gmail.com>
2016-01-23 22:27:53 +0530

    URL encoding done

--- moviemon.py
+++ moviemon.py
@@ -3,6 +3,8 @@ import urllib2
 
 from guessit import guess_file_info
 
+from urllib import urlencode
+
 from config import PATHS
 
 OMDB_URL = 'http://www.omdbapi.com/?'
@@ -17,33 +19,40 @@ EXT = (".3g2 .3gp .3gp2 .3gpp .60d .ajp .asf .asx .avchd .avi .bik .bix"
 
 EXT = tuple(EXT.split())
 
-movies = []
-
 
 def scan_dir(path):
     for root, dirs, files in os.walk(path):
             for name in files:
                 path = os.path.join(root, name)
-                if os.path.getsize(path) > (25*1024*1024):
+                if os.path.getsize(path) > (25*1024*1024):    # split this into separate func.
                     ext = os.path.splitext(name)[1]
                     if ext in EXT:
-                        print name
                         get_movie_info(name)
                         print "\n"
-                        # movies.append(movie_name)
-
-# print movies
 
 
-# def omdb():
-#   """ Fetch data from OMDB API. """
-#     url = OMDB_URL +
+def omdb(title, year):
+    """ Fetch data from OMDB API. """
+    params = {'t': title.encode('ascii', 'ignore'),
+              'plot': 'full',
+              'type': 'movie',
+              'tomatoes': 'true'}
+
+    if year:
+        params['y'] = year
+
+    url = OMDB_URL + urlencode(params)
+    print url
 
 
 def get_movie_info(name):
     """Find movie information"""
-    movie_name = guess_file_info(name)
-    print movie_name
+    movie_info = guess_file_info(name)
+    if movie_info['type'] == "movie":    # what if I have TV shows in that folder?
+        if movie_info.has_key('year'):
+            omdb(movie_info['title'], movie_info['year'])
+        else:
+            omdb(movie_info['title'], None)
 
 
 if __name__ == '__main__':
